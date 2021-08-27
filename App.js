5c3ae0b615603d0fde4a935fa2435440641c5522
Davi Fernando Rosa <davir0514@gmail.com>
2021-08-26 21:58:21 -0300

    Arquivos

---
+++ App.js
@@ -0,0 +1,60 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+
+   const [playing, setPlaying] = useState(false);
+
+   const HEIGHT = 500;
+   const WIDTH = 500;
+
+   const startVideo = () =>{
+      setPlaying(true)
+      navigator.getUserMedia(
+        {
+          video: true,
+        },
+        (stream) => {
+          let video = document.getElementsByClassName('app_videoFeed')[0];
+          if(video){
+            video.srcObject = stream;
+          }
+        },
+         (err)=> console.error(err)
+      );
+   };
+
+   const stopVideo = () =>{
+      setPlaying(false)
+      let video = document.getElementsByClassName('app_videoFeed')[0];
+      video.srcObject.getTracks()[0].stop();
+   };
+
+
+  return (
+    <div className="App">
+        <h2 className='title'>CAPTURA WEBCAM</h2>
+         <div className='app_container'>
+             <video
+              className='app_videoFeed'
+              height = {HEIGHT}
+              width = {WIDTH}
+              muted
+              autoPlay
+            ></video>
+         </div>
+
+           <div className='app_input'>
+               {playing ? (
+                 <button className='btnStop' onClick={stopVideo}>Stop</button>
+                 ): (
+                 <button className='btnStart' onClick={startVideo}>Start</button>
+                 )}      
+           </div>
+
+
+    </div>
+  );
+}
+
+export default App;
